7c2e303c8a41577f940bf79f19a77ef8d1197462
Zach Reed <zachreed@air-watch.com>
2015-08-28 17:01:26 -0400

    writing new updates to file

--- PageChanges.py
+++ PageChanges.py
@@ -1,21 +1,21 @@
 import requests
+import subprocess
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParser
-
+import datetime
 
 url = "https://slack.com/jobs"
 data = requests.get(url)
 content = ""
 
-#Download page data
+# Download page data
 if data.raise_for_status() != None:
 	print "Error encountered"
 	content = data.raise_for_status()
 else:
 	content = data.text
 
-#Parse out relevant content
-
+# Parse out relevant content & write to file
 class ZHTMLStrip(HTMLParser):
 	def __init__(self):
 		self.reset()
@@ -30,10 +30,38 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data() 
 
+timeStamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 soup = BeautifulSoup(content, 'html.parser')
 myH4s = str(soup.findAll('h4')).split(", ")
-for i in range(len(myH4s)):
-	print strip_tags(myH4s[i])
 
-	
+# Check file count in Results Directory
+ps = subprocess.Popen(('ls', 'Results/'), stdout=subprocess.PIPE)
+fileCount = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+ps.wait()
+
+def write_file():
+	f = open('Results/results_' + timeStamp + '.txt', 'w+')
+	for i in range(len(myH4s)):
+		f.write(strip_tags(myH4s[i] + "\n"))
+	f.close()
+
+if int(fileCount) < 2:
+	write_file()
+else:
+	print "too bad bro"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
