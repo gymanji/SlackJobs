d366308b98f84f0c98888b719205b13b11fcf221
Zach Reed <zachreed@air-watch.com>
2015-08-28 15:44:09 -0400

    clean up

--- PageChanges.py
+++ PageChanges.py
@@ -1,39 +1,35 @@
-
 import requests
 from bs4 import BeautifulSoup
+import re
 
-#https://slack.com/jobs
-#http://blogs.air-watch.com/
 
 url = "https://slack.com/jobs"
 data = requests.get(url)
+content = ""
 
+#Download page data
 if data.raise_for_status() != None:
 	print "Error encountered"
 	content = data.raise_for_status()
 else:
 	content = data.text
 
+#Parse out relevant content
 soup = BeautifulSoup(content, 'html.parser')
-
 mydivs = soup.findAll("a", { "class" : "posting-title" })
 items = str(mydivs).split(", ")
 
 finalData = []
-for item in items:
-	print item
+for i in range(len(items)):
+	stripData = re.match('<h4>*?</h4>', items[i])
+	finalData.append(stripData)	
+
+print finalData
+# finalData = []
+# for item in items:
+# 	stripData = re.match('<h4>*', item)
+# 	finalData.append(stripData)
+
+# print finalData
 	
 
-
-
-
-
-# for i in len(mydivs):
-# 	print mydivs[i] + "\n"
-
-
-
-# from subprocess import call
-#call(["curl", "http://blogs.air-watch.com/ > data2.txt"])
-# file = open('data.txt', 'r')
-# file.read()
