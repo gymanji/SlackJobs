5c4f1994ae7d794b5c3f4f8a2ced2fb2da4b9659
Zach Reed <ZachReed@air-watch.com>
2015-08-30 21:51:25 -0400

    saving any changes before running updates

--- PageChanges.py
+++ PageChanges.py
@@ -35,11 +35,6 @@ def strip_tags(html):
 soup = BeautifulSoup(content, 'html.parser')
 myH4s = str(soup.findAll('h4')).split(", ")
 
-# Check file count in Results Directory
-ps = subprocess.Popen(('ls', 'Results/'), stdout=subprocess.PIPE)
-fileCount = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
-ps.wait()
-
 # Determine oldest file in Results Directory
 resultPath = '/Users/Zach/Development/GitHub Repos/SlackJobs/Results/'
 os.chdir(resultPath)
@@ -47,29 +42,28 @@ files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 oldest = files[0]
 
 def write_file(myH4s):
-	timeStamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-	f = open('Results/result_' + timeStamp + '.txt', 'w')
+	timeStamp = datetime.datetime.now().strftime("%H%M%S")
+	f = open('Results/result-' + timeStamp + '.txt', 'w')
 	for i in range(len(myH4s)):
 		f.write(strip_tags(myH4s[i] + "\n"))
 	f.close()
 
+# Check file count in Results Directory
+ps = subprocess.Popen(('ls', 'Results/'), stdout=subprocess.PIPE)
+fileCount = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+ps.wait()	
 
-# print int(fileCount)
-
-# write_file(myH4s)
-
-if int(fileCount) < 6:
-	write_file(myH4s)
-else:
-	print "file count over 2"
-	os.remove(resultPath + oldest)
+# Write new file or delete oldest and create new
+# if int(fileCount) < 2:
+# 	write_file(myH4s)
+# else:
+# 	print "file count over 2"
+# 	os.remove(resultPath + oldest)
+# 	write_file(myH4s)
 	
-
-
-
-
-
-
+# Check diff between two job files
+files = []
+# print subprocess.call(['ls', resultPath])
 
 
 
