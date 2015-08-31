021d31df4b40da7f3b57ed2691fabf4a39b41688
Zach Reed <zachreed@air-watch.com>
2015-08-31 16:52:41 -0400

    cleaned up some formatting; diff still in progress; added function to email results

--- PageChanges.py
+++ PageChanges.py
@@ -1,18 +1,19 @@
 import requests
-import subprocess
 from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
-import datetime
+from datetime import datetime
 import os
+from os.path import isfile, join
+import difflib
 
 
 # Download page data
-url = "https://slack.com/jobs"
+url = 'https://slack.com/jobs'
 data = requests.get(url)
-content = ""
+content = ''
 
 if data.raise_for_status() != None:
-	print "Error encountered"
+	print 'Error encountered'
 	content = data.raise_for_status()
 else:
 	content = data.text
@@ -34,36 +35,74 @@ def strip_tags(html):
 
 soup = BeautifulSoup(content, 'html.parser')
 myH4s = str(soup.findAll('h4')).split(", ")
-
 # Determine oldest file in Results Directory
 resultPath = '/Users/zreed/SlackJobs/Results/'
-os.chdir(resultPath)
-files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+files = [ f for f in os.listdir(resultPath) if isfile(join(resultPath,f)) ]
 oldest = files[0]
+fileCount = int(len(files))
 
 def write_file(myH4s):
-	timeStamp = datetime.datetime.now().strftime("%H%M%S")
+	timeStamp = datetime.now().strftime("%H%M%S")
 	f = open(resultPath + 'result-' + timeStamp + '.txt', 'w')
 	for i in range(len(myH4s)):
 		f.write(strip_tags(myH4s[i] + "\n"))
 	f.close()
 
-# Check file count in Results Directory
-ps = subprocess.Popen(('ls', resultPath), stdout=subprocess.PIPE)
-fileCount = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
-ps.wait()	
-
 # Write new file or delete oldest and create new
-if int(fileCount) < 2:
+if fileCount < 3:
 	write_file(myH4s)
 else:
-	print "file count over 2"
 	os.remove(resultPath + oldest)
 	write_file(myH4s)
-	print "wrote new file"
-# Check diff between two job files
-files = []
-# print subprocess.call(['ls', resultPath])
+
+# Check diff between two files
+files = [ f for f in os.listdir(resultPath) if isfile(join(resultPath,f)) ]
+fileContents = ['','']
+
+# print open(resultPath + files[0]).readlines()
+
+# for i in range(2):
+# 	with open(resultPath + str(files[i])) as f:
+# 		for line in f:
+# 			fileContents[i] += line
+a = open(resultPath + files[0]).readlines()
+b = open(resultPath + files[1]).readlines()
+
+# for line in difflib.unified_diff(a, b, n=0):
+#     print line
+    # for prefix in ('---', '+++', '@@'):
+    #     if line.startswith(prefix):
+    #         break
+    # else:
+    #     print line
+
+# print changes
+
+# Send any changes via email
+def send_email(user, pwd, recipient, subject, body):
+	import smtplib
+
+	gmail_user = user
+	gmail_pwd = pwd
+	FROM = user
+	TO = recipient if type(recipient) is list else [recipient]
+	SUBJECT = subject
+	TEXT = body
+
+	message = '\From: %s\nTo: %s\nSubject: %s\n\n%s""' % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+	try:
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.ehlo()
+		server.starttls()
+		server.login(gmail_user, gmail_pwd)
+		server.close()
+		print 'email successfully sent'
+	except:
+		print 'email failed to send'
+
+
+send_email('', '', '', 'Python Email', 'Email is here!')
 
 
 
