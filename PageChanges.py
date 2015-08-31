c046bfb830bb457b1cc4a006d0e9b58e5fe0abb6
Zach Reed <zachreed@air-watch.com>
2015-08-31 09:48:30 -0400

    properly writing txt files with job logs; improperly formatted path was the issue

--- PageChanges.py
+++ PageChanges.py
@@ -36,31 +36,31 @@ soup = BeautifulSoup(content, 'html.parser')
 myH4s = str(soup.findAll('h4')).split(", ")
 
 # Determine oldest file in Results Directory
-resultPath = '/Users/Zach/Development/GitHub Repos/SlackJobs/Results/'
+resultPath = '/Users/zreed/SlackJobs/Results/'
 os.chdir(resultPath)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 oldest = files[0]
 
 def write_file(myH4s):
 	timeStamp = datetime.datetime.now().strftime("%H%M%S")
-	f = open('Results/result-' + timeStamp + '.txt', 'w')
+	f = open(resultPath + 'result-' + timeStamp + '.txt', 'w')
 	for i in range(len(myH4s)):
 		f.write(strip_tags(myH4s[i] + "\n"))
 	f.close()
 
 # Check file count in Results Directory
-ps = subprocess.Popen(('ls', 'Results/'), stdout=subprocess.PIPE)
+ps = subprocess.Popen(('ls', resultPath), stdout=subprocess.PIPE)
 fileCount = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
 ps.wait()	
 
 # Write new file or delete oldest and create new
-# if int(fileCount) < 2:
-# 	write_file(myH4s)
-# else:
-# 	print "file count over 2"
-# 	os.remove(resultPath + oldest)
-# 	write_file(myH4s)
-	
+if int(fileCount) < 2:
+	write_file(myH4s)
+else:
+	print "file count over 2"
+	os.remove(resultPath + oldest)
+	write_file(myH4s)
+	print "wrote new file"
 # Check diff between two job files
 files = []
 # print subprocess.call(['ls', resultPath])
