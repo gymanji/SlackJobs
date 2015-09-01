c40cb82fa018d6aee435461ed0102fbbc16b5e19
Zach Reed <ZachReed@air-watch.com>
2015-08-31 22:09:00 -0400

    not pretty but functional from the standpoint of emailing if latest page has new additions

--- PageChanges.py
+++ PageChanges.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import os
 from os.path import isfile, join
-import difflib
 
 
 # Download page data
@@ -35,8 +34,9 @@ def strip_tags(html):
 
 soup = BeautifulSoup(content, 'html.parser')
 myH4s = str(soup.findAll('h4')).split(", ")
+
 # Determine oldest file in Results Directory
-resultPath = '/Users/zreed/SlackJobs/Results/'
+resultPath = '/Users/Zach/Development/GitHub Repos/SlackJobs/Results/'
 files = [ f for f in os.listdir(resultPath) if isfile(join(resultPath,f)) ]
 oldest = files[0]
 fileCount = int(len(files))
@@ -45,7 +45,7 @@ def write_file(myH4s):
 	timeStamp = datetime.now().strftime("%H%M%S")
 	f = open(resultPath + 'result-' + timeStamp + '.txt', 'w')
 	for i in range(len(myH4s)):
-		f.write(strip_tags(myH4s[i] + "\n"))
+		f.write(strip_tags(myH4s[i] + '\n'))
 	f.close()
 
 # Write new file or delete oldest and create new
@@ -55,51 +55,41 @@ else:
 	os.remove(resultPath + oldest)
 	write_file(myH4s)
 
+# Send any changes via email
+def send_email(toaddr, data):
+	import smtplib
+	from email.MIMEMultipart import MIMEMultipart
+	from email.MIMEText import MIMEText
+	 
+	fromaddr = 'add_here'
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = 'Slack Page Changes'
+	body = 'Page additions:\n\n' + data
+	msg.attach(MIMEText(body, 'plain'))
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, 'add_here')
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
+
 # Check diff between two files
 files = [ f for f in os.listdir(resultPath) if isfile(join(resultPath,f)) ]
-fileContents = ['','']
+a = set(open(resultPath + files[0]).readlines())
+b = set(open(resultPath + files[1]).readlines())
+c = set(open(resultPath + files[2]).readlines())
 
-# print open(resultPath + files[0]).readlines()
-
-# for i in range(2):
-# 	with open(resultPath + str(files[i])) as f:
-# 		for line in f:
-# 			fileContents[i] += line
-a = open(resultPath + files[0]).readlines()
-b = open(resultPath + files[1]).readlines()
-
-# for line in difflib.unified_diff(a, b, n=0):
-#     print line
-    # for prefix in ('---', '+++', '@@'):
-    #     if line.startswith(prefix):
-    #         break
-    # else:
-    #     print line
-
-# print changes
-
-# Send any changes via email
-
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
- 
-fromaddr = ''
-toaddr = ''
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = 'Python Email'
- 
-body = 'and here is the data!'
-msg.attach(MIMEText(body, 'plain'))
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, '')
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
-
+oldestDiff = b - a
+newestDiff = c - b
+if oldestDiff == newestDiff:
+	print 'no changes'
+else:
+	mailData = ''
+	for i in newestDiff:
+		mailData += i
+	print 'sending mail...'
+	send_email('add_here', mailData)
 
 
